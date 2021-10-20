fb61bace003f382541c1199750e9b5c0f5db30c0
CullenWagner <cullen.mwagner@gmail.com>
2021-10-20 18:01:56 -0500

    removed unncessary import

--- src/main/groovy/com/jmuraski/toDoApp/ToDoList/ToDoListConfig.java
+++ src/main/groovy/com/jmuraski/toDoApp/ToDoList/ToDoListConfig.java
@@ -1,28 +1,28 @@
-package com.jmuraski.toDoApp.ToDoList;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class ToDoListConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(ToDoListRepository toDoListRepository) {
-        return args -> {
-            ToDoList list1 = new ToDoList(
-                    "Monday List"
-            );
-
-            ToDoList list2 = new ToDoList(
-                    "Tuesday List"
-            );
-
-            toDoListRepository.saveAll(
-                    List.of(list1, list2)
-            );
-        };
-    }
-}
+//package com.jmuraski.toDoApp.ToDoList;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class ToDoListConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(ToDoListRepository toDoListRepository) {
+//        return args -> {
+//            ToDoList list1 = new ToDoList(
+//                    "Monday List"
+//            );
+//
+//            ToDoList list2 = new ToDoList(
+//                    "Tuesday List"
+//            );
+//
+//            toDoListRepository.saveAll(
+//                    List.of(list1, list2)
+//            );
+//        };
+//    }
+//}
